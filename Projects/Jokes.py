08d9132d0eb412fe2bf75970002df99075e15ac9
Planktonnnnn <willemwinkelhake@seattleacademy.org>
2026-03-13 21:09:32 +0000

    Sync Changes

---
+++ Projects/Jokes.py
@@ -0,0 +1,19 @@
+name = input("What is your name?")
+print(f"hello {name} and welcome to CT8")
+
+ 
+
+
+
+print("why did the chicken cross the road?")
+input()
+print("To hear all of your own punchlines")
+ 
+
+input()
+
+
+print("what do you call a fish with no eyes")
+input()
+print("a fsh")
+
